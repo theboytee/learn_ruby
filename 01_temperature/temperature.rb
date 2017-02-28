0d2e1460ba23695f739c23ec7dcadd58669f96bd
Toba <toba.adewuyi@yahoo.com>
2017-02-28 03:11:30 +0000

    Added completed exercises 00, 01 and 02

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,7 @@
-#write your code here
+def ftoc(fahrenheit)
+  (fahrenheit - 32.0)*(5.0/9.0)
+end
+
+def ctof(celsius)
+  (9.0/5.0)*celsius +32.0
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,29 @@
-#write your code here
+def add(a, b)
+ a+b
+end
+
+def subtract(a, b)
+ a-b
+end
+
+def sum(array)
+  total = 0
+  array.each { |number| total += number }
+  total
+end
+
+def multiply (a, b)
+  a*b
+end
+
+def power (a, b)
+  a**b
+end
+
+def factorial (number)
+  if (number == 1) || (number == 0)
+    return 1
+  else
+    return number*factorial(number-1)
+  end
+end
